998d95a0cb4858d91259b2b0086f1befcb11c17c
Tman Barnes <Trystan.Barnes@outlook.com>
2022-06-02 13:19:40 -0700

    * added dependency for puppeteer * added a puppeteer sample (emulate Iphone6 browsing to nyt mlb page)

--- puppeteer1.js
+++ puppeteer1.js
@@ -1,10 +1,12 @@
 const puppeteer = require('puppeteer');
+const iPhone = puppeteer.devices['iPhone 6'];
 
 (async () => {
   const browser = await puppeteer.launch({headless: false, slowmo:100});
   const page = await browser.newPage();
-  await page.goto('https://www.nytimes.com');
-  await page.screenshot({ path: 'example.png' });
+  await page.emulate(iPhone);
+  await page.goto('https://www.nytimes.com/section/sports/baseball');
+  await page.screenshot({ path: 'example1.png' });
 
   await browser.close();
 })();
